1c7abd28a0665bd26f118c50bd267b993ffd0c1c
Parinthorn Saithong <parinthorn.sa@gmail.com>
2016-11-11 09:45:30 +0700

    Connect to real server

--- server.js
+++ server.js
@@ -4,58 +4,68 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+// GraphQL Client
+var Lokka = require('lokka').Lokka;
+var Transport = require('lokka-transport-http').Transport;
+
+const headers = {
+  'Accept': 'application/json'
+};
+const client = new Lokka({
+  transport: new Transport('http://localhost:8080', {
+    headers
+  })
+});
+
 var COMMENTS_FILE = path.join(__dirname, 'comments.json');
 
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // Additional middleware which will set headers that we need on each request.
 app.use(function(req, res, next) {
-    // Set permissive CORS header - this allows this server to be used only as
-    // an API server in conjunction with something like webpack-dev-server.
-    res.setHeader('Access-Control-Allow-Origin', '*');
+  // Set permissive CORS header - this allows this server to be used only as
+  // an API server in conjunction with something like webpack-dev-server.
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Disable caching so we'll always get the latest comments.
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
+  // Disable caching so we'll always get the latest comments.
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
 });
 
 app.get('/api/comments', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
+  client.query(`
+    {
+      comments {
+        id,
+        author,
+        text
+      }
     }
-    res.json(JSON.parse(data));
+  `).then((query) => {
+    res.json(query.comments);
+    console.log(query.comments);
   });
 });
 
 app.post('/api/comments', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
+  client.mutate(`{
+    newComment: add(
+      author: "${req.body.author}",
+      text: "${req.body.text}"
+    ) {
+      id,
+      author,
+      text,
     }
-    var comments = JSON.parse(data);
-    // NOTE: In a real implementation, we would likely rely on a database or
-    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
-    // treat Date.now() as unique-enough for our purposes.
-    var newComment = {
-      id: Date.now(),
-      author: req.body.author,
-      text: req.body.text,
-    };
-    comments.push(newComment);
-    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-      res.json(comments);
-    });
+  }`).then(mutation => {
+    console.log(mutation.newComment);
+    res.json(mutation.newComment);
   });
 });
 
